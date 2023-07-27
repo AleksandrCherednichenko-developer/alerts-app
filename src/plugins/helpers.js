9f5b69402a75f3a09dff97c4aafe28548b095e93
AleksandrCherednichenko <acherednichenko174@gmail.com>
2023-07-27 23:53:33 +0700

    feat: -Adding new functionality files;

--- src/plugins/helpers.js
+++ src/plugins/helpers.js
@@ -16,3 +16,16 @@ export const getQueryParams = (data = {}) => {
 
     return urlSearchParams.toString();
 };
+
+// export mockArrItems = Array.from({ length: 5 }, (_, i) => `image-${i + 1}.png`);// использовал бы этот код если были бы все изображения
+export const mockArrItems = Array.from({ length: 26 }, (_, i) => `image${(i % 5) + 1}`);
+
+export function splitArrayToRows (array, row = 3) {
+    const firstPartLength = Math.floor(array.length / row) - 1;
+    const secondPartLength = firstPartLength + 1;
+    const thirdPartLength = firstPartLength;
+    const firstPart = array.slice(0, firstPartLength);
+    const secondPart = array.slice(firstPartLength, firstPartLength + secondPartLength);
+    const thirdPart = array.slice(firstPartLength + secondPartLength, firstPartLength + secondPartLength + thirdPartLength);
+    return [firstPart, secondPart, thirdPart];
+}
